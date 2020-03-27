447d0d58d50a82097b728eb34809aad20cca1f3a
alvez <joaor4e@gmail.com>
2020-03-26 23:32:12 -0300

    finished the backend integration to mobile front-end and finished font-end mobile

---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,70 @@
+import {StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
+export default StyleSheet.create({
+  DetailContainer: {
+    flex:1,
+    paddingHorizontal:24,
+    paddingTop: Constants.statusBarHeight + 20
+  },
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+  },
+  incident:{
+    padding:24,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    marginBottom: 16,
+    marginTop: 48
+  },
+  IncidentProperty:{
+    fontSize: 14,
+    color: '#41414d',
+    marginTop:24,
+    fontWeight: 'bold'
+  },
+  IncidentValue:{
+    marginTop:8,
+    fontSize:15,
+    color: '#737380'
+  },
+  contactBox:{
+      padding:24,
+      borderRadius: 8,
+      backgroundColor: '#fff',
+      marginBottom: 16,
+  },
+  HeroTitle:{
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: '#13131a',
+    lineHeight: 30,
+    textAlign: 'center'
+  },
+  HeroDescription:{
+    fontSize:14,
+    color: '#737380',
+    marginTop: 16,
+    textAlign:'center'
+  },
+  actions:{
+    marginTop:16,
+    flexDirection:'row',
+    justifyContent: 'space-between'
+  },
+  action:{
+    backgroundColor:'#e02041',
+    borderRadius: 8,
+    height: 50,
+    width: '48%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  actionText:{
+    color:'#fff',
+    fontWeight: 'bold',
+    fontSize:15
+  }
+})
---
+++ mobile/src/pages/Incidents/styles.js
@@ -0,0 +1,64 @@
+import {StyleSheet} from 'react-native';
+import Constants from "expo-constants";
+
+export default StyleSheet.create({
+  container: {
+    flex:1,
+    paddingHorizontal: 24,
+    paddingTop: Constants.statusBarHeight + 20,
+  },
+  header:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  headerText:{
+    fontSize:15,
+    color: '#737380',
+  },
+  headerTextBold:{
+    fontWeight: 'bold',
+  },
+  title:{
+    fontSize:30,
+    marginBottom: 16,
+    marginTop: 48,
+    color: '#13131a',
+    fontWeight: 'bold',
+  },
+  description:{
+    fontSize:16,
+    lineHeight:24,
+    color: '#737380',
+  },
+  incidentList:{
+    marginTop: 32,
+  },
+  incident:{
+    padding:24,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+    marginBottom: 16
+  },
+  IncidentProperty:{
+    fontSize: 14,
+    color: '#41414d',
+    fontWeight: 'bold'
+  },
+  IncidentValue:{
+    marginTop:8,
+    fontSize:15,
+    marginBottom:24,
+    color: '#737380'
+  },
+  DetailsButton:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems: 'center'
+  },
+  DetailsButtonText:{
+    color: '#e02041',
+    fontWeight: 'bold',
+    fontSize:15
+  }
+});
